c001ce98c9704d84da679de616920316680058ff
Jamie Bowman <mrjamiebowman@gmail.com>
2020-09-20 20:08:37 -0400

    updates to apim demo for docker

--- azure-apim-docker/start.ps1
+++ azure-apim-docker/start.ps1
@@ -1,9 +1,9 @@
 
 # create bridge network
-docker network create --driver bridge apim-demo
+docker network create --driver bridge --subnet=172.20.0.0/16 --gateway=172.20.0.1 apim-demo
 
 # remove existing docker container
-docker rm -f local-apim-demo2
+docker rm -f local-apim-demo
 
 # run container on apim-demo network
-docker run -d -p 80:8080 -p 443:8081 --name local-apim-demo2 --network apim-demo --env-file env.conf mcr.microsoft.com/azure-api-management/gateway:latest
+docker run -d -p 80:8080 -p 443:8081 --name local-apim-demo --network apim-demo --env-file env.conf mcr.microsoft.com/azure-api-management/gateway:latest
